7c08000a9e81ba4f520d5cd86362f04d8afda351
Kirk Morris <worker.kirk@gmail.com>
2022-11-22 11:38:49 -0800

    Installs capacitor core, cli, and Android libraries.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saricden.relicReclaimation',
+  appName: 'Relic Reclaim',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
